c840e6070e8577de081adea731f8975a139e3bc8
iihadi <abdulhadi2422@outlook.com>
2019-04-07 12:59:32 +0100

    Added weather button, pug and js file

---
+++ routes/weather.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const request = require('request');
+
+
+const router = express.Router();
+
+// About page
+router.get("/", (req, res) => {
+  res.render("weather");
+});
+
+
+module.exports = router;
